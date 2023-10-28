046709559ccac1e1a7638a6e3923768a6b6a1456
Andre Costa <andre2396costa@gmail.com>
2023-10-28 15:11:02 -0300

    Final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,17 @@
+import { Pet } from './../models/Pet';
+import { createMenuObject } from './../helpers/createMenuObject';
 import { Request, Response } from "express";
 
 export const search = (req: Request, res: Response) => {
-    res.render('pages/page');
+    let query = req.query.q as string;
+    if (!query) {
+        res.redirect('/');
+        return;
+    }
+    let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
